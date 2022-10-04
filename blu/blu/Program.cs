756aeebfa441331fd47fb39fd57b7f37b08abe11
florentbenech <florentbenech@gmail.com>
2022-10-04 15:09:41 +0200

    ticket 002 c1

--- blu/blu/Program.cs
+++ blu/blu/Program.cs
@@ -12,6 +12,7 @@ namespace blu
         {
             Console.WriteLine("Ticket001");
             Console.WriteLine("Ticket001_2");
+            Console.WriteLine("Ticket002_1");
         }
     }
 }
